615463ae431abb78825f1699d3e8aea0e2cdaac8
Nouhayla Erraou <nouhaylaerraou@gmail.com>
2024-02-22 21:56:51 +0000

    feat: country users

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,8 @@
+-- create users table if not exists
+-- use enums
+CREATE TABLE IF NOT EXISTS users (
+    id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    name VARCHAR(255),
+    country ENUM('US', 'CO', 'TN') DEFAULT 'US'
+)
