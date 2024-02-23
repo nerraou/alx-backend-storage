a85a83161e138aa941c0cb6f4459708f6d066e81
Nouhayla Erraou <nouhaylaerraou@gmail.com>
2024-02-23 20:34:54 +0000

    feat: div function

---
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -0,0 +1,17 @@
+-- safe div
+-- create function
+
+DELIMITER $$
+
+CREATE FUNCTION SafeDiv (a INT, b INT)
+RETURNS FLOAT DETERMINISTIC
+BEGIN
+    IF (b = 0)
+    THEN
+        RETURN (0);
+    END IF;
+
+    RETURN a / b;
+END$$
+
+DELIMITER ;
