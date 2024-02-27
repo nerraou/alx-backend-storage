e0529ff4b2927d7a05aecc04eab90f4d2270867e
Nouhayla Erraou <nouhaylaerraou@gmail.com>
2024-02-27 23:15:14 +0000

    feat(python): update topics

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" 10-update_topics """
+
+
+def update_topics(mongo_collection, name, topics):
+    """ update topics """
+    mongo_collection.update_many(
+        {"name": name},
+        {"$set": {"topics": topics}}
+    )
