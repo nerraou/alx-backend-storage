f90b841cef4c2327cef9b7814411f82a3d009bcc
Nouhayla Erraou <nouhaylaerraou@gmail.com>
2024-02-27 23:02:39 +0000

    fix(python): remove type annotation

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python3
 """ 9-insert_school """
-from pymongo.collection import Collection
 
 
-def insert_school(mongo_collection: Collection, **kwargs):
+def insert_school(mongo_collection, **kwargs):
     """ insert school """
     record = mongo_collection.insert_one(kwargs)
     return record.inserted_id
