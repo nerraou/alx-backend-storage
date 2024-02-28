4d8c902dce70718e7e2c73c7ba0202a936cb5c10
Nouhayla Erraou <nouhaylaerraou@gmail.com>
2024-02-29 00:23:42 +0100

    create storage

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+""" Redis with python
+"""
+import redis
+from typing import Union
+from uuid import uuid4
+
+
+class Caches:
+    """
+    This is cash class
+    """
+    def __init__(self) -> None:
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data: Union[str, bytes,  int,  float]) -> str:
+        """
+        The method should generate a random key
+        """
+        key = str(uuid4())
+        client = self._redis
+        client.set(key, data)
+        return key
