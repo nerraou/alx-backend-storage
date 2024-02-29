c8657a973c47f88d70046b3391f2013ebaa8eae4
Nouhayla Erraou <nouhaylaerraou@gmail.com>
2024-02-29 22:26:18 +0100

    decorate count calls

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -34,6 +34,7 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @count_calls
     def store(self, data: Union[str, bytes,  int,  float]) -> str:
         """
         The method should generate a random key
