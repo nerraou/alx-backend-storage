8eae97cd06eb7ff231a3621cb4a29ee780ca1ff4
Nouhayla Erraou <nouhaylaerraou@gmail.com>
2024-03-01 21:49:42 +0000

    feat: cache the result

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -8,6 +8,8 @@ from functools import wraps
 from typing import Callable
 from uuid import uuid4
 
+redis_client = redis.Redis()
+
 
 def track_url_access(method: Callable) -> Callable:
     """
@@ -18,11 +20,19 @@ def track_url_access(method: Callable) -> Callable:
         """
         Wrapper function.
         """
-        key = "count:{}".format(args[0])
-        redis_client = redis.Redis()
+        url = args[0]
+
+        key = "count:{}".format(url)
         redis_client.incr(key)
 
-        return method(*args)
+        cached_result = redis_client.get("cached:{}".format(url))
+        if cached_result:
+            return cached_result.decode("utf-8")
+
+        html = method(url)
+        redis_client.setex("cached:{}".format(url), 10, html)
+
+        return html
 
     return wrapper
 
@@ -32,4 +42,4 @@ def get_page(url: str) -> str:
     """
     obtain the HTML content of a particular URL and return it
     """
-    return requests.get(url)
+    return requests.get(url).text
