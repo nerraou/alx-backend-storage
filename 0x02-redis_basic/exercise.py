f2bdbf5a933e588bbf83b533746e00fc121b7ce1
Nouhayla Erraou <nouhaylaerraou@gmail.com>
2024-03-01 21:05:33 +0000

    feat: add call_history decorator

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -8,19 +8,42 @@ from uuid import uuid4
 
 
 def count_calls(method: Callable) -> Callable:
-    '''
-     function that increments the count for
-       that key every time the method is called and returns
-       the value returned by the original method.
-    '''
+    """
+    function that increments the count for
+    that key every time the method is called
+    """
     @wraps(method)
     def wrapper(self, *args, **kwargs):
-        '''
-            Wrapper function.
-        '''
+        """
+        Wrapper function.
+        """
         key = method.__qualname__
         self._redis.incr(key)
+
         return method(self, *args, **kwargs)
+
+    return wrapper
+
+
+def call_history(method: Callable) -> Callable:
+    """
+    function that store method inputs and output using
+    RPUSH command
+    """
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        """
+        Wrapper function.
+        """
+        value = method(self, *args, **kwargs)
+
+        inputs_key = "{}:inputs".format(method.__qualname__)
+        outputs_key = "{}:outputs".format(method.__qualname__)
+        self._redis.rpush(inputs_key, str(args))
+        self._redis.rpush(outputs_key, str(value))
+
+        return value
+
     return wrapper
 
 
@@ -36,6 +59,7 @@ class Cache:
         self._redis.flushdb()
 
     @count_calls
+    @call_history
     def store(self, data: Union[str, bytes,  int,  float]) -> str:
         """
         The method should generate a random key
